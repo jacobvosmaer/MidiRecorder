b5744f204c1d0470e8c124f6930e0d2f0b58d7fe
Jacob Vosmaer <contact@jacobvosmaer.nl>
2023-11-22 16:06:48 +0100

    It records!

--- MidiRecorder.cpp
+++ MidiRecorder.cpp
@@ -1,4 +1,3 @@
-#include <string.h>
 #include "daisy_pod.h"
 #include "fatfs.h"
 #include <string.h>
@@ -13,9 +12,14 @@ using namespace daisy;
 DaisyPod hw;
 
 uint8_t DSY_SDRAM_BSS buffer[(1 << 26)]; /* Use all 64MB of sample RAM */
+struct {
+  uint8_t *start, *end, *pos;
+} buf = {buffer, buffer + sizeof(buffer), buffer};
 
 SdmmcHandler sd;
 FatFSInterface fsi;
+
+MidiUartTransport midi;
 
 void mount(const char *path) {
   SdmmcHandler::Config sd_cfg;
@@ -25,26 +29,59 @@ void mount(const char *path) {
   assert(!f_mount(&fsi.GetSDFileSystem(), path, 1));
 }
 
-char msg[512];
+int recording = 1;
+void midicallback(uint8_t *data, size_t size, void *context) {
+  if (recording && ((ptrdiff_t)size < buf.end - buf.pos)) {
+    memcpy(buf.pos, data, size);
+    buf.pos += size;
+  }
+}
+
+char obuf[512];
+
+void ledred(void) {
+  hw.led1.SetRed(1);
+  hw.UpdateLeds();
+}
 
 int main(void) {
   FIL file = {0};
-  unsigned nw;
 
   hw.Init();
+
   mount("/");
   assert(!f_open(&file, "uart.bin", FA_CREATE_ALWAYS | FA_WRITE));
 
-  memset(msg, '\n', sizeof(msg));
-  sprintf(msg, "hello world!\n");
-  assert(!f_write(&file, msg, sizeof(msg), &nw));
-  assert(!f_close(&file));
+  midi.Init(MidiUartTransport::Config());
+  midi.StartRx(midicallback, 0);
 
-  int recording = 1;
   while (1) {
     hw.ProcessDigitalControls();
+
     if (recording && hw.button1.RisingEdge()) {
+      uint8_t *p;
+      unsigned nw;
+      enum { frame = 510 };
       recording = 0;
+
+      for (p = buf.start; buf.pos - p >= frame; p += frame) {
+        obuf[0] = 512 >> 8;
+        obuf[1] = 512 & 0xff;
+        memcpy(obuf + 2, p, frame);
+        f_write(&file, obuf, sizeof(obuf), &nw);
+      }
+
+      int rem = buf.pos - p;
+      if (rem) {
+        obuf[0] = rem >> 8;
+        obuf[1] = rem & 0xff;
+        memcpy(obuf + 2, p, rem);
+        memset(obuf + 2 + rem, 0, frame - rem);
+        f_write(&file, obuf, sizeof(obuf), &nw);
+      }
+
+      f_close(&file);
+      ledred();
     }
   }
 }
