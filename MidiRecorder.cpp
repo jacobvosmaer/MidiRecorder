e41b5c0becfcc761e9ba7cca34d66ba7ec47eb84
Jacob Vosmaer <contact@jacobvosmaer.nl>
2023-11-22 14:51:00 +0100

    Can write file but only multiples of 512

--- MidiRecorder.cpp
+++ MidiRecorder.cpp
@@ -1,13 +1,12 @@
+#include <string.h>
 #include "daisy_pod.h"
 #include "fatfs.h"
+#include <string.h>
 
 #undef assert
 #define assert(x)                                                              \
   if (!(x))                                                                    \
   asm("bkpt 255")
-
-int ferr;
-#define assert_ff(x) assert((ferr = (x)) == FR_OK)
 
 using namespace daisy;
 
@@ -16,15 +15,17 @@ DaisyPod hw;
 uint8_t DSY_SDRAM_BSS buffer[(1 << 26)]; /* Use all 64MB of sample RAM */
 
 SdmmcHandler sd;
-SdmmcHandler::Config sd_cfg;
 FatFSInterface fsi;
 
 void mount(const char *path) {
+  SdmmcHandler::Config sd_cfg;
   sd_cfg.Defaults();
   sd.Init(sd_cfg);
   fsi.Init(FatFSInterface::Config::MEDIA_SD);
-  assert_ff(f_mount(&fsi.GetSDFileSystem(), path, 1));
+  assert(!f_mount(&fsi.GetSDFileSystem(), path, 1));
 }
+
+char msg[512];
 
 int main(void) {
   FIL file = {0};
@@ -32,16 +33,18 @@ int main(void) {
 
   hw.Init();
   mount("/");
-  assert_ff(f_open(&file, "uart.bin", FA_CREATE_ALWAYS | FA_WRITE));
+  assert(!f_open(&file, "uart.bin", FA_CREATE_ALWAYS | FA_WRITE));
 
-  assert_ff(f_write(&file, "hello world\n", 12, &nw));
-  assert_ff(f_sync(&file));
+  memset(msg, '\n', sizeof(msg));
+  sprintf(msg, "hello world!\n");
+  assert(!f_write(&file, msg, sizeof(msg), &nw));
+  assert(!f_close(&file));
+
   int recording = 1;
   while (1) {
     hw.ProcessDigitalControls();
     if (recording && hw.button1.RisingEdge()) {
       recording = 0;
-      f_close(&file);
     }
   }
 }
