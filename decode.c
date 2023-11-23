a427f9f4b4a4dcd40ddaa7acc7cac43a7313975e
Jacob Vosmaer <contact@jacobvosmaer.nl>
2023-11-23 15:14:52 +0100

    decode: validate header

--- decode.c
+++ decode.c
@@ -6,6 +6,10 @@ int main(void) {
   while (fread(buf, sizeof(buf), 1, stdin)) {
     unsigned char *p = buf + 2;
     int n = (int)buf[0] << 8 | (int)buf[1];
+    if (n < 2 || n > sizeof(buf)) {
+      fprintf(stderr, "invalid header: %d\n", n);
+      return 1;
+    }
     n -= 2;
     while (n--)
       putchar(*p++);
