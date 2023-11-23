9dcbe4d1a98ad3418b86cb5bece3045ee8af85db
Jacob Vosmaer <contact@jacobvosmaer.nl>
2023-11-23 15:12:03 +0100

    print: rename variable

--- print.c
+++ print.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
 int main(void) {
-  int ch, n;
-  for (n = 0; (ch = getchar()) != EOF; n++) {
-    if (n && (ch & 0x80))
+  int ch, first = 1;
+  while ((ch = getchar()) != EOF) {
+    if (!first && (ch & 0x80))
       putchar('\n');
+    first = 0;
     printf("%02x ", ch);
   }
-  if (n)
+  if (!first)
     putchar('\n');
 }
